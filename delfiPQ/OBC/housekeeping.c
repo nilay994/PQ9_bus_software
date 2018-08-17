4efad7250e0f96ca1b1521c1cf9ee67a1a4d883c
nchronas <nchronas@gmail.com>
2018-08-17 16:55:34 +0200

    Fixed message size

--- delfiPQ/OBC/housekeeping.c
+++ delfiPQ/OBC/housekeeping.c
@@ -5,11 +5,22 @@
 #include "packet_utilities.h"
 #include "parameters.h"
 
-
 uint8_t adb_hk_buffer[255];
 uint8_t adcs_hk_buffer[255];
 uint8_t eps_hk_buffer[255];
 uint8_t comms_hk_buffer[255];
+
+uint8_t adb_hk_size = 0;
+uint8_t adcs_hk_size = 0;
+uint8_t eps_hk_size = 0;
+uint8_t comms_hk_size = 0;
+
+void clr_hk_storage() {
+  adb_hk_size = 0;
+  adcs_hk_size = 0;
+  eps_hk_size = 0;
+  comms_hk_size = 0;
+}
 
 void populate_housekeeping(uint8_t *buf, uint8_t *pkt_size) {
 
@@ -53,12 +64,16 @@ void store_housekeeping(SBSYS_id id, uint8_t *buf, uint16_t size) {
 
   if(id == ADB_APP_ID) {
     memcpy(adb_hk_buffer, buf, size);
+    adb_hk_size = size;
   } else if(id == ADCS_APP_ID) {
     memcpy(adcs_hk_buffer, buf, size);
+    adcs_hk_size = size;
   } else if(id == EPS_APP_ID) {
     memcpy(eps_hk_buffer, buf, size);
+    eps_hk_size = size;
   } else if(id == COMMS_APP_ID) {
     memcpy(comms_hk_buffer, buf, size);
+    comms_hk_size = size;
   }
 
 }
@@ -66,13 +81,17 @@ void store_housekeeping(SBSYS_id id, uint8_t *buf, uint16_t size) {
 void retrieve_housekeeping(SBSYS_id id, uint8_t *buf, uint16_t *size) {
 
   if(id == ADB_APP_ID) {
-    memcpy(buf, adb_hk_buffer, size);
+    memcpy(buf, adb_hk_buffer, adb_hk_size);
+    *size = adb_hk_size;
   } else if(id == ADCS_APP_ID) {
-    memcpy(buf, adcs_hk_buffer, size);
+    memcpy(buf, adcs_hk_buffer, adcs_hk_size);
+    *size = adcs_hk_size;
   } else if(id == EPS_APP_ID) {
-    memcpy(buf, eps_hk_buffer, size);
+    memcpy(buf, eps_hk_buffer, eps_hk_size);
+    *size = eps_hk_size;
   } else if(id == COMMS_APP_ID) {
-    memcpy(buf, comms_hk_buffer, size);
+    memcpy(buf, comms_hk_buffer, comms_hk_size);
+    *size = comms_hk_size;
   } else if(id == OBC_APP_ID) {
     buf[0] = SYSTEM_APP_ID;
     populate_housekeeping(&buf[1], size);
