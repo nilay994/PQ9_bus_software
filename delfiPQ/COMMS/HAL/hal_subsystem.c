6c481620f86133e383c1da271fffb7864ccedba8
nchronas <nchronas@gmail.com>
2018-10-04 13:48:38 +0200

    Fix for rs485 lost packets issue

--- delfiPQ/COMMS/HAL/hal_subsystem.c
+++ delfiPQ/COMMS/HAL/hal_subsystem.c
@@ -84,7 +84,7 @@ void HAL_peripheral_open() {
   uartParams.readDataMode = UART_DATA_BINARY;
   uartParams.readReturnMode = UART_RETURN_FULL;
   uartParams.readEcho = UART_ECHO_OFF;
-  uartParams.baudRate = 500000;
+  uartParams.baudRate = 115200; //500000;
   uartParams.readCallback = &temp;
   uart_pq9_bus = UART_open(PQ9, &uartParams);
 
